3700abe8233b5402a1d023366ecc77ac843b98b8
hieuct <hieuct@huongvietgroup.com>
2022-01-25 17:45:30 +0700

    commit

--- LibDemo/Areas/Admin/Controllers/HomeController.cs
+++ LibDemo/Areas/Admin/Controllers/HomeController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Models;
+using Models.Dao.DaoAdmin;
+using Models.EF;
 
 namespace LibDemo.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        DonViDao dv = new DonViDao();
+        NhanVienDao nv = new NhanVienDao();
         // GET: Admin/Home
         public ActionResult Index()
         {
@@ -15,6 +20,10 @@ namespace LibDemo.Areas.Admin.Controllers
             {
                 return RedirectToAction("Index", "Login");
             }
+            long IDNV = long.Parse(Session["IDNV"].ToString());
+            int IDDV = int.Parse(Session["IDDonVi"].ToString());
+            ViewBag.UserInfo = nv.getDataByID(IDNV);
+            ViewBag.DonViInfo = dv.getDataByID(IDDV);
             return View();
         }
     }
