0d52c3e6dde5f83d5a0ebade360fd7228b8a7d3f
hungnh <hungsan1995@gmail.com>
2022-01-26 10:56:11 +0700

    update template client

---
+++ LibDemo/Controllers/LibraryListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibDemo.Controllers
+{
+    public class LibraryListController : Controller
+    {
+        // GET: LibraryList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LibDemo/Controllers/NewspaperController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibDemo.Controllers
+{
+    public class NewspaperController : Controller
+    {
+        // GET: Newspaper
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
